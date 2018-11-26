2eeecc0e37ecf1a6baf5d6e01247815e23831c13
changyuanchn <changyuanchn@gmail.com>
2018-11-26 23:47:05 +0800

    结合公共状态context，使得每个子组件都可以修改storee

--- reduxDemo/redux-demo/src/Header.js
+++ reduxDemo/redux-demo/src/Header.js
@@ -2,9 +2,34 @@ import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 
 class Header extends Component {
+    // 添加访问共享状态store的一些前提
+    static contextTypes = {
+        store: PropTypes.object
+    }
+
+    constructor(){
+        super()
+        this.state = {themeColor:''}
+    }
+
+    _updateThemeColor(){
+        const {store} = this.context
+        const state = store.getState()
+        this.setState(
+            {
+                themeColor: state.themeColor
+            }
+        )
+    }
+    componentWillMount(){
+        const {store} = this.context
+        this._updateThemeColor()
+        store.subscribe(()=>this._updateThemeColor())
+    }
+
     render(){
         return (
-            <h1>React Header</h1>
+            <h1 style={{color:this.state.themeColor}}>React Header</h1>
         )
     }
 }
--- reduxDemo/redux-demo/src/index.js
+++ reduxDemo/redux-demo/src/index.js
@@ -6,6 +6,15 @@ import Content from './Content'
 import './index.css'
 
 class Index extends Component{
+
+    // 标准写法，才能使子组件能够访问到共享状态store
+    static childContextTypes = {
+        store : PropTypes.object
+    }
+    getChildContext(){
+        return {store}
+    }
+
     render(){
         return (
             <div>
@@ -15,5 +24,36 @@ class Index extends Component{
         )
     }
 }
+
+const themeReducer = (state, action) =>{
+    if(!state) return {
+        themeColor:'red'
+    }
+    switch(action.type){
+        case 'CHANGE_COLOR' :
+          return {
+              ...state,
+              themeColor:action.themeColor
+          }
+        default:
+          return state
+    }
+}
+
+function createStore(reducer){
+    let state=null
+    const listeners = []
+    const getState = ()=> state
+    const subscribe = (listener) => listeners.push(listener)
+    const dispatch = (action) =>{
+        state = reducer(state,action)
+        listeners.forEach( (listener) => listener() )
+    }
+    dispatch({}) // initialise state
+    return {getState, dispatch, subscribe}
+}
+
+// 这里定义一下共享状态store
+const store = createStore(themeReducer)
 
 ReactDOM.render(<Index />, document.getElementById('root'));
