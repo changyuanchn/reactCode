7c43f8b8b30b1b7a148647e691ce9e2cf32864ac
changyuanchn <changyuanchn@gmail.com>
2018-11-28 22:24:33 +0800

    提供一个Provider容器，把index割离开来

--- reduxDemo/redux-demo/src/index.js
+++ reduxDemo/redux-demo/src/index.js
@@ -1,20 +1,13 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+
 import ReactDOM from 'react-dom'
 import Header from './Header'
 import Content from './Content'
 import './index.css'
+import { Provider } from './react-redux';
 
 class Index extends Component{
-
-    // 标准写法，才能使子组件能够访问到共享状态store
-    static childContextTypes = {
-        store : PropTypes.object
-    }
-    getChildContext(){
-        return {store}
-    }
-
+    
     render(){
         return (
             <div>
@@ -56,4 +49,8 @@ function createStore(reducer){
 // 这里定义一下共享状态store
 const store = createStore(themeReducer)
 
-ReactDOM.render(<Index />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+      <Index />
+    </Provider>,
+     document.getElementById('root'));
--- reduxDemo/redux-demo/src/react-redux.js
+++ reduxDemo/redux-demo/src/react-redux.js
@@ -38,3 +38,29 @@ export const connect = (mapStateToProps,mapDispatchToProps) =>(WrappedComponent)
     }
     return Connect
 }
+
+// 添加Provider类，来清理index.js，这样就把context从index里面割离开来了
+// Provider 做的事情也很简单，它就是一个容器组件，会把嵌套的内容原封不动作为自己的子组件渲染出来。它还会把外界传给它的 props.store 放到 context，这样子组件 connect 的时候都可以获取到。
+export class Provider extends Component{
+    static propTypes = {
+        store : PropTypes.object,
+        children: PropTypes.any
+    }
+
+    static childContextTypes = {
+        store:PropTypes.object
+    }
+
+    getChildContext(){
+        return {
+            store: this.props.store
+        }
+    }
+
+    render (){
+        return (
+            <div>{this.props.children}</div>
+        )
+    }
+
+}
