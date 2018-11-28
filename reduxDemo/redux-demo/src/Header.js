32e9b29a86baa3bd575f2095f1cc8bfbf31de221
changyuanchn <changyuanchn@gmail.com>
2018-11-28 21:58:58 +0800

    利用高阶组件抽取公共部分，利用connect连接高阶组件和Dumb组组件

--- reduxDemo/redux-demo/src/Header.js
+++ reduxDemo/redux-demo/src/Header.js
@@ -1,37 +1,26 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
+import { connect } from './react-redux';
 
 class Header extends Component {
     // 添加访问共享状态store的一些前提
     static contextTypes = {
-        store: PropTypes.object
-    }
-
-    constructor(){
-        super()
-        this.state = {themeColor:''}
-    }
-
-    _updateThemeColor(){
-        const {store} = this.context
-        const state = store.getState()
-        this.setState(
-            {
-                themeColor: state.themeColor
-            }
-        )
-    }
-    componentWillMount(){
-        const {store} = this.context
-        this._updateThemeColor()
-        store.subscribe(()=>this._updateThemeColor())
+        themeColor: PropTypes.string
     }
 
     render(){
         return (
-            <h1 style={{color:this.state.themeColor}}>React Header</h1>
+            <h1 style={{color:this.props.themeColor}}>React Header</h1>
         )
     }
 }
+
+const mapStateToProps = (state)=>{
+    return {
+        themeColor: state.themeColor
+    }
+}
+
+Header = connect(mapStateToProps)(Header)
 
 export default Header
