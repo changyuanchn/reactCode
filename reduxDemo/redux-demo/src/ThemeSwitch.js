31a093bfeab5119787e4a6ee1978d1257ed3b48b
changyuanchn <changyuanchn@gmail.com>
2018-11-28 22:14:19 +0800

    添加mapDispatchToProps来完善connect，进而重构ThemeSwitch.js

--- reduxDemo/redux-demo/src/ThemeSwitch.js
+++ reduxDemo/redux-demo/src/ThemeSwitch.js
@@ -1,41 +1,49 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { connect } from './react-redux';
 
 class ThemeSwitch extends Component{
     static contextTypes={
-        store:PropTypes.object
+        themeColor:PropTypes.string,
+        onSwitchColor:PropTypes.func
     }
     constructor(){
         super()
         this.state = {themeColor : ''}
     }
-    componentWillMount(){
-        const {store} = this.context
-        this._updateThemeColor()
-        store.subscribe(()=>this._updateThemeColor())
-    }
-    _updateThemeColor(){
-        const {store} = this.context
-        const state = store.getState()
-        this.setState({themeColor:state.themeColor})
-    }
+
     handleSwitchColor(color){
-        const {store} = this.context
-        store.dispatch({
-            type:'CHANGE_COLOR',
-            themeColor:color
-        })
+        if(this.props.onSwitchColor){
+            this.props.onSwitchColor(color)
+        }
     }
     render(){
         return(
             <div>
-                <button style={{color:this.state.themeColor}} 
+                <button style={{color:this.props.themeColor}} 
                 onClick={this.handleSwitchColor.bind(this, 'red')}>Red</button>
-                <button style={{color:this.state.themeColor}} 
+                <button style={{color:this.props.themeColor}} 
                 onClick={this.handleSwitchColor.bind(this, 'blue')}>blue</button>
             </div>
         )
     }
 }
+
+// 在这里不仅要获取context的内容，还要修改，所以之前的connect是不够用的，因此需要多加一个修改函数
+const mapDispatchToProps = (dispatch)=>{
+    return {
+        onSwitchColor:(color)=>{
+            dispatch({type:'CHANGE_COLOR', themeColor:color})
+        }
+    }
+}
+
+const mapStateToProps = (state) =>{
+    return {
+        themeColor: state.themeColor
+    }
+}
+
+ThemeSwitch = connect(mapStateToProps, mapDispatchToProps)(ThemeSwitch)
 
 export default ThemeSwitch
